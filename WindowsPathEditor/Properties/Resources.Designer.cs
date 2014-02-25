ca5c290da5f906f101814212efdf280675b14c40
LeoColomb <LeoColomb@users.noreply.github.com>
2014-02-25 23:02:38 +0100

    New add Icon

--- WindowsPathEditor/Properties/Resources.Designer.cs
+++ WindowsPathEditor/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace WindowsPathEditor.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap delete {
             get {
                 object obj = ResourceManager.GetObject("delete", resourceCulture);
